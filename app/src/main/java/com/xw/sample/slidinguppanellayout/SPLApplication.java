13cf35b856e43ae8d667b8e3c5b22298ed815d52
woxingxiao <Chauvet910@gmail.com>
2017-07-14 14:48:12 +0800

    fix #1

---
+++ app/src/main/java/com/xw/sample/slidinguppanellayout/SPLApplication.java
@@ -0,0 +1,17 @@
+package com.xw.sample.slidinguppanellayout;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
+
+/**
+ * <p/>
+ * Created by woxingxiao on 2017-07-14.
+ */
+
+public class SPLApplication extends Application {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+}
